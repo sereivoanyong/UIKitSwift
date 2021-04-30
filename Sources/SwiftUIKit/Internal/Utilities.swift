30190e50ead396168b8bc52335ab0a6ddf63b8fd
Sereivoan Yong <sereivoanyong@gmail.com>
2021-04-30 18:49:41 +0700

    Add `EmptyView`

---
+++ Sources/SwiftUIKit/Internal/Utilities.swift
@@ -0,0 +1,23 @@
+//
+//  Utilities.swift
+//
+//  Created by Sereivoan Yong on 4/30/21.
+//
+
+#if os(iOS)
+
+import ObjectiveC
+
+@usableFromInline
+func class_exchangeInstanceMethodImplementations(_ cls: AnyClass, _ originalSelector: Selector, _ swizzledSelector: Selector) {
+  let originalMethod = class_getInstanceMethod(cls, originalSelector).unsafelyUnwrapped
+  let swizzledMethod = class_getInstanceMethod(cls, swizzledSelector).unsafelyUnwrapped
+  let wasMethodAdded = class_addMethod(cls, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+  if wasMethodAdded {
+    class_replaceMethod(cls, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+  } else {
+    method_exchangeImplementations(originalMethod, swizzledMethod)
+  }
+}
+
+#endif
