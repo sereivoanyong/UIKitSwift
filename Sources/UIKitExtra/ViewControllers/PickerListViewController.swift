e4cb50c08af8bbe110ad573f43c76259e036a901
Sereivoan Yong <sereivoanyong@gmail.com>
2022-03-10 16:57:20 +0700

    Make `PickerListViewController` bahavior-based

--- Sources/UIKitExtra/ViewControllers/PickerListViewController.swift
+++ Sources/UIKitExtra/ViewControllers/PickerListViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-// TODO: search support & section/item modeling rework
+// TODO: section/item modeling rework, search support & deselection support
 
 @available(iOS 14.0, *)
 open class PickerListViewController<Item>: CollectionViewController, UICollectionViewDataSource, UICollectionViewDelegate {
@@ -16,10 +16,33 @@ open class PickerListViewController<Item>: CollectionViewController, UICollectio
     public let items: [Item]
   }
 
+  public enum Behavior {
+
+    /// Exit via navigation `backButtonItem` or pop gesture
+    /// - `saveAndShowHandler` is invoked on selection.
+    case pushWithSaveAndShowChangeOnSelection(saveAndShowHandler: (Item) -> Void) // appearance, font, etc.
+
+    /// Exit via `doneButtonItem`
+    /// - `saveAndShowHandler` is invoked on selection.
+    case presentWithSaveAndShowChangeOnSelection(saveAndShowHandler: (Item) -> Void)
+
+    /// Exit via `cancelButtonItem` and save/dismiss via `doneButtonItem`
+    /// - `saveHandler` is invoked on `doneButtonItem` tapped
+    /// - `showHandler` is invoked on selection
+    /// - `restoreHandler` is invoked on `cancelButtonItem` tapped.
+    case presentWithShowChangeOnSelection(saveHandler: (Item) -> Void, showHandler: (Item) -> Void, restoreHandler: () -> Void)
+
+    /// Exit via `cancelButtonItem`
+    /// - `saveHandler` is invoked on selection.
+    case presentWithSaveChangeOnSelectionThenDismiss(saveHandler: (Item) -> Void)
+  }
+
   lazy open private(set) var doneButtonItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done(_:)))
   lazy open private(set) var cancelButtonItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel(_:)))
 
   private let equalityProvider: (Item, Item) -> Bool
+
+  public let behavior: Behavior
 
   open var sections: [Section] {
     didSet {
@@ -31,7 +54,7 @@ open class PickerListViewController<Item>: CollectionViewController, UICollectio
 
   open var selectedItem: Item?
 
-  public var contentProvider: (Item) -> UIListContentConfiguration = { item in
+  open var contentProvider: (Item) -> UIListContentConfiguration = { item in
     var content = UIListContentConfiguration.cell()
     content.text = "\(item)"
     return content
@@ -43,29 +66,18 @@ open class PickerListViewController<Item>: CollectionViewController, UICollectio
     }
   }
 
-  public var selectionCancellationHandler: (() -> Void)?
-
-  /// If this property is not nil, it'll be invoked whenever selection changes and user must tap done to save and dismiss
-  public var selectionHandler: ((Item) -> Void)? {
-    didSet {
-      navigationItem.rightBarButtonItem = selectionHandler != nil ? doneButtonItem : nil
-    }
+  public convenience init(items: [Item], behavior: Behavior) where Item: Identifiable {
+    self.init(equalityProvider: { $0.id == $1.id }, items: items, behavior: behavior)
   }
 
-  public let doneHandler: (Item) -> Void
-
-  public convenience init(items: [Item], doneHandler: @escaping (Item) -> Void) where Item: Identifiable {
-    self.init(equalityProvider: { $0.id == $1.id }, items: items, doneHandler: doneHandler)
+  public convenience init(items: [Item], behavior: Behavior) where Item: Equatable {
+    self.init(equalityProvider: ==, items: items, behavior: behavior)
   }
 
-  public convenience init(items: [Item], doneHandler: @escaping (Item) -> Void) where Item: Equatable {
-    self.init(equalityProvider: ==, items: items, doneHandler: doneHandler)
-  }
-
-  private init(equalityProvider: @escaping (Item, Item) -> Bool, items: [Item], doneHandler: @escaping (Item) -> Void) {
+  private init(equalityProvider: @escaping (Item, Item) -> Bool, items: [Item], behavior: Behavior) {
     self.equalityProvider = equalityProvider
     self.sections = [.init(items: items)]
-    self.doneHandler = doneHandler
+    self.behavior = behavior
     super.init(nibName: nil, bundle: nil)
 
     navigationItem.largeTitleDisplayMode = .never
@@ -107,22 +119,50 @@ open class PickerListViewController<Item>: CollectionViewController, UICollectio
 
   open override func willMove(toParent parent: UIViewController?) {
     if let navigationController = parent as? UINavigationController, navigationController.viewControllers.count == 1 {
-      navigationItem.leftBarButtonItem = cancelButtonItem
+      switch behavior {
+      case .pushWithSaveAndShowChangeOnSelection:
+        break
+      case .presentWithSaveAndShowChangeOnSelection:
+        navigationItem.rightBarButtonItem = doneButtonItem // to exit
+      case .presentWithShowChangeOnSelection:
+        navigationItem.rightBarButtonItem = doneButtonItem // to save then dismiss
+        navigationItem.leftBarButtonItem = cancelButtonItem // to exit
+      case .presentWithSaveChangeOnSelectionThenDismiss:
+        navigationItem.leftBarButtonItem = cancelButtonItem // to exit
+      }
     }
     super.willMove(toParent: parent)
   }
 
   @objc open func done(_ sender: Any?) {
-    guard let selectedItem = selectedItem else {
-      return
+    switch behavior {
+    case .pushWithSaveAndShowChangeOnSelection:
+      fatalError()
+    case .presentWithSaveAndShowChangeOnSelection:
+      dismiss(animated: true, completion: nil)
+    case .presentWithShowChangeOnSelection(let saveHandler, _, _):
+      guard let selectedItem = selectedItem else {
+        return
+      }
+      saveHandler(selectedItem)
+      dismiss(animated: true, completion: nil)
+    case .presentWithSaveChangeOnSelectionThenDismiss:
+      fatalError()
     }
-    doneHandler(selectedItem)
-    dismiss(animated: true, completion: nil)
   }
 
   @objc open func cancel(_ sender: UIBarButtonItem) {
-    selectionCancellationHandler?()
-    dismiss(animated: true, completion: nil)
+    switch behavior {
+    case .pushWithSaveAndShowChangeOnSelection:
+      fatalError()
+    case .presentWithSaveAndShowChangeOnSelection:
+      fatalError()
+    case .presentWithShowChangeOnSelection(_, _, let restoreHandler):
+      restoreHandler()
+      dismiss(animated: true, completion: nil)
+    case .presentWithSaveChangeOnSelectionThenDismiss:
+      dismiss(animated: true, completion: nil)
+    }
   }
 
   @inlinable
@@ -153,10 +193,17 @@ open class PickerListViewController<Item>: CollectionViewController, UICollectio
   open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     let newSelectedItem = item(at: indexPath)
     selectedItem = newSelectedItem
-    if let selectionHandler = selectionHandler {
-      selectionHandler(newSelectedItem)
-    } else {
-      done(collectionView.cellForItem(at: indexPath))
+
+    switch behavior {
+    case .pushWithSaveAndShowChangeOnSelection(let saveAndShowHandler):
+      saveAndShowHandler(newSelectedItem)
+    case .presentWithSaveAndShowChangeOnSelection(let saveAndShowHandler):
+      saveAndShowHandler(newSelectedItem)
+    case .presentWithShowChangeOnSelection(_, let showHandler, _):
+      showHandler(newSelectedItem)
+    case .presentWithSaveChangeOnSelectionThenDismiss(let saveHandler):
+      saveHandler(newSelectedItem)
+      dismiss(animated: true, completion: nil)
     }
   }
 }
