cf11a70b7c3c85387e44070e8679b51684565647
Sereivoan Yong <sereivoanyong@gmail.com>
2023-04-09 12:20:00 +0700

    Bump swift-tools-version to 5.7

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
   name: "UIKitSwift",
   platforms: [
-    .iOS(.v10)
+    .iOS(.v11)
   ],
   products: [
     .library(name: "UIKitSwift", targets: ["UIKitSwift"]),
